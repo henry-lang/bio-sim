ee087d03e2503198dc58daa307d11187969157e4
Henry Langmack <Henry.langmack@gmail.com>
2021-10-31 23:32:36 -0400

    A lot of stuff@

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class Predator:
         self.skill = skill
         self.eaten = True
 
+    def get_children(self) -> list["Predator"]:
+        return [Predator(self.skill + 1), Predator(self.skill - 1)]
+
     def __repr__(self) -> str:
         return f"{str(self.skill)} {str(self.eaten)}"
 
@@ -19,6 +22,9 @@ class Prey:
 
     def __init__(self, skill: int):
         self.skill = skill
+
+    def get_children(self) -> list["Prey"]:
+        return [Prey(self.skill + 1), Prey(self.skill - 1)]
 
     def __repr__(self) -> str:
         return str(self.skill)
@@ -60,11 +66,12 @@ class Square:
 
     def simulate(self):
         while (best_predator := self.find_best_predator()) and (best_prey := self.find_best_prey(best_predator)):
-            print(f"{best_predator} eats {best_predator}")
-            self.predators.append(Predator(best_predator.skill - 1))
-            self.predators.append(Predator(best_predator.skill + 1))
+            self.predators += best_predator.get_children()
             self.prey.remove(best_prey)
             self.predators.remove(best_predator)
+
+        self.prey = [prey.get_children() for prey in self.prey]
+        self.prey = [item for sublist in self.prey for item in sublist]
 
     def __repr__(self) -> str:
         return f"[Predators: {str(self.predators)}, Prey: {str(self.prey)}]"
@@ -72,12 +79,14 @@ class Square:
 
 class Simulation:
     size: int
+    round: int
     board: list[Square]
     distribution: list[tuple[int, int]]
 
     def __init__(self, size: int, distribution: list[tuple[int, int]]):
         self.board = [Square() for _ in range(size)]
         self.size = size
+        self.round = 0
         self.distribution = distribution
 
         self.populate()
@@ -91,9 +100,36 @@ class Simulation:
                 self.random_square().predators.append(Predator(skill))
                 self.random_square().prey.append(Prey(skill))
 
+    def shuffle(self):
+        all_predators: list[Predator] = []
+        all_prey: list[Prey] = []
+
+        for square in self.board:
+            all_predators += square.predators
+            all_prey += square.prey
+            square.predators = []
+            square.prey = []
+
+        for predator in all_predators:
+            self.random_square().predators.append(predator)
+        for prey in all_prey:
+            self.random_square().prey.append(prey)
+
+    def get_stats(self) -> str:
+        predator_count = 0
+        prey_count = 0
+        for square in self.board:
+            predator_count += len(square.predators)
+            prey_count += len(square.prey)
+        return f"Predators: {str(predator_count)} | Prey: {str(prey_count)}"
+
     def simulate(self):
+        print(f"Before round {self.round}: {self.get_stats()}")
         for square in self.board:
             square.simulate()
+        print(f"After round {self.round}: {self.get_stats()}")
+        self.shuffle()
+        self.round += 1
 
     def __repr__(self) -> str:
         return str(self.board)
@@ -102,9 +138,8 @@ class Simulation:
 def main():
     distribution = [(2, 1), (3, 2), (4, 3), (5, 4), (6, 3), (7, 2), (8, 1)]
     sim = Simulation(25, distribution)
-    print(sim)
-    sim.simulate()
-    print(sim)
+    for _ in range(100):
+        sim.simulate()
 
 
 main()
